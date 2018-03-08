b0b49055fafa1b0d23791e4a31deed4c36072c05
Anton Mykhaylov <>
2018-03-08 13:55:30 -0600

    created initial file structure

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('jr. bacon cheeseburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('angus steak burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('royal with cheese', false);
+
